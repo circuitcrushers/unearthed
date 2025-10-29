d4b94f095009af5973ee589c1efe5508e8710cae
Circuit Crushers <circuit.crushers.fll@gmail.com>
2025-10-28 20:32:35 -0400

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -5,7 +5,7 @@ selected = hub_menu("1", "2")
 
 # Based on the selection, run a program.
 if selected == "1":
-    import Mission_5_6_8
+    import Mission_5_6_8_del
 elif selected == "2":
-    import Mission_10
+    import Mission_9_10
 
