84b189ad25c472f096102465174412f7fd51a726
Circuit Crushers <circuit.crushers.fll@gmail.com>
2025-11-21 22:22:19 -0500

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -1,15 +1,17 @@
 from pybricks.tools import hub_menu
 
 # Menu selection
-selected = hub_menu("1", "2", "3", "4")
+selected = hub_menu("1", "2", "3", "4", "5")
 
 # Based on the selection, run a program.
 if selected == "1":
     import Mission_5_6_7_8
 elif selected == "2":
-    import Mission_9_10 
+    import Mission_9_10_1
 elif selected == "3":
     import SalvageOperation
 elif selected == "4":
-    import MapReveal_1
+    import MapReveal
+elif selected == "5":
+    import Careful_Recovery_1    
 
